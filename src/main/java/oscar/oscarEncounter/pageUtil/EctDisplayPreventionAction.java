0f828cb520af7a2301d0e4d3dff326c4b20c4ffd
Michael Yingbull <michael@maplecreekmedical.ca>
2022-06-10 23:46:03 -0400

    Updates for highlighting preventions, OscarPro ports and tweaks.

--- src/main/java/oscar/oscarEncounter/pageUtil/EctDisplayPreventionAction.java
+++ src/main/java/oscar/oscarEncounter/pageUtil/EctDisplayPreventionAction.java
@@ -36,6 +36,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts.util.MessageResources;
 import org.oscarehr.caisi_integrator.ws.CachedDemographicPrevention;
+import org.oscarehr.common.dao.BillingONItemDao;
+import org.oscarehr.common.model.BillingONItem;
 import org.oscarehr.util.LoggedInInfo;
 import org.oscarehr.util.SpringUtils;
 
@@ -50,144 +52,155 @@ import oscar.util.StringUtils;
  *
  */
 public class EctDisplayPreventionAction extends EctDisplayAction {
-    private static final String cmd = "preventions";
+    	private static final String cmd = "preventions";
 
-    public boolean getInfo(EctSessionBean bean, HttpServletRequest request, NavBarDisplayDAO Dao, MessageResources messages) {
-    	LoggedInInfo loggedInInfo=LoggedInInfo.getLoggedInInfoFromSession(request);
-		
-		if (!securityInfoManager.hasPrivilege(loggedInInfo, "_prevention", "r", null)) {
-    		return true; //Prevention link won't show up on new CME screen.
-    	} else {
+    	public boolean getInfo(EctSessionBean bean, HttpServletRequest request, NavBarDisplayDAO Dao, MessageResources messages) {
 
-        //set lefthand module heading and link
-        String winName = "prevention" + bean.demographicNo;
-        int demographicNumber = Integer.valueOf(bean.demographicNo);
-        String url = "popupPage(700,960,'" + winName + "', '" + request.getContextPath() + "/oscarPrevention/index.jsp?demographic_no=" + bean.demographicNo + "')";
-        Dao.setLeftHeading(messages.getMessage(request.getLocale(), "oscarEncounter.LeftNavBar.Prevent"));
-        Dao.setLeftURL(url);
+		LoggedInInfo loggedInInfo=LoggedInInfo.getLoggedInInfoFromSession(request);
 
-        //set righthand link to same as left so we have visual consistency with other modules
-        url += ";return false;";
-        Dao.setRightURL(url);
-        Dao.setRightHeadingID(cmd);  //no menu so set div id to unique id for this action
+                if (!securityInfoManager.hasPrivilege(loggedInInfo, "_prevention", "r", null)) {
+                	return true; //Prevention link won't show up on new CME screen.
+        	} else {
 
-        //list warnings first as module items
-        Prevention p = PreventionData.getPrevention(loggedInInfo, Integer.valueOf(bean.demographicNo));
-        PreventionDS pf = SpringUtils.getBean(PreventionDS.class);//PreventionDS.getInstance();
+        		//set lefthand module heading and link
+        		String winName = "prevention" + bean.demographicNo;
+			int demographicNumber = Integer.valueOf(bean.demographicNo);
+        		String url = "popupPage(700,960,'" + winName + "', '" + request.getContextPath() + "/oscarPrevention/index.jsp?demographic_no=" + bean.demographicNo + "')";
+        		Dao.setLeftHeading(messages.getMessage(request.getLocale(), "oscarEncounter.LeftNavBar.Prevent"));
+        		Dao.setLeftURL(url);
 
-        try{
-            pf.getMessages(p);
-        }catch(Exception dsException){
-            return false;
-        }
+        		//set righthand link to same as left so we have visual consistency with other modules
+        		url += ";return false;";
+        		Dao.setRightURL(url);
+        		Dao.setRightHeadingID(cmd);  //no menu so set div id to unique id for this action
 
-        //now we list prevention modules as items
-        PreventionDisplayConfig pdc = PreventionDisplayConfig.getInstance();
-        ArrayList<HashMap<String,String>> prevList = pdc.getPreventions();
-        Map warningTable = p.getWarningMsgs();
+        		//list warnings first as module items
+        		Prevention p = PreventionData.getPrevention(loggedInInfo, Integer.valueOf(bean.demographicNo));
+        		PreventionDS pf = SpringUtils.getBean(PreventionDS.class);//PreventionDS.getInstance();
 
-        String highliteColour = "#FF0000";
-        String inelligibleColour = "#FF6600";
-        String pendingColour = "#FF00FF";
-        Date date = null;
+        		try {
+            			pf.getMessages(p);
+        		} catch (Exception dsException) {
+            			return false;
+        		}	
 
-        url += "; return false;";
-        ArrayList<NavBarDisplayDAO.Item> warnings = new ArrayList<NavBarDisplayDAO.Item>();
-        ArrayList<NavBarDisplayDAO.Item> items = new ArrayList<NavBarDisplayDAO.Item>();
-        String result;
-        Date demographicDateOfBirth=PreventionData.getDemographicDateOfBirth(loggedInInfo, demographicNumber);
+        		//now we list prevention modules as items
+        		PreventionDisplayConfig pdc = PreventionDisplayConfig.getInstance();
+        		ArrayList<HashMap<String,String>> prevList = pdc.getPreventions();
+        		Map warningTable = p.getWarningMsgs();
 
-        // fetch and cache any remote integrated preventions.
-        List<CachedDemographicPrevention> integratedPreventions = null;
-        List<CachedDemographicPrevention> remotePreventions = PreventionData.getRemotePreventions(loggedInInfo, demographicNumber);
-        
-        if(remotePreventions.size() > 0)
-        {
-        	integratedPreventions = new ArrayList<CachedDemographicPrevention>();
-        	integratedPreventions.addAll(remotePreventions);
-        }
+        		BillingONItemDao billingONItemDao = SpringUtils.getBean(BillingONItemDao.class);
+        		List<String> exclusions = billingONItemDao.getPreventionExclusionsByDemographicNo(Integer.valueOf(bean.demographicNo));
 
-        for (int i = 0 ; i < prevList.size(); i++){
-            NavBarDisplayDAO.Item item = NavBarDisplayDAO.Item();
-            HashMap<String,String> h = prevList.get(i);
-            String prevName = h.get("name");
-            ArrayList<Map<String,Object>> alist = PreventionData.getPreventionData(loggedInInfo, prevName, demographicNumber);
-            
-            if(integratedPreventions != null) {
-            	PreventionData.addRemotePreventions(loggedInInfo, integratedPreventions, alist, prevName,demographicDateOfBirth);
-            }
-            
-            boolean show = pdc.display(loggedInInfo, h, bean.demographicNo,alist.size());
-            if( show ) {
-                if( alist.size() > 0 ) {
-                    Map<String,Object> hdata = alist.get(alist.size()-1);
-                    Map<String,String> hExt = PreventionData.getPreventionKeyValues((String)hdata.get("id"));
-                    result = hExt.get("result");
-                    
-                    Object dateObj = hdata.get("prevention_date_asDate");
-                    if(dateObj instanceof Date){
-                    	date = (Date) dateObj;
-                    }else if(dateObj instanceof java.util.GregorianCalendar){
-                    	Calendar cal = (Calendar) dateObj;
-                    	date = cal.getTime();
-                    }
-                    
-                    item.setDate(date);
+        		String abnormalColour = "#EE5F5B";
+        		String highliteColour = "#FF0000";
+        		String inelligibleColour = "#E6AC00";
+        		String pendingColour = "#FF00FF";
+        		String refusedColour = "#EAACAC";       //light pink
+        		Date date = null;
 
-                    if( hdata.get("refused") != null && hdata.get("refused").equals("2") ) {
-                        item.setColour(inelligibleColour);
-                    }
-                    else if( result != null && result.equalsIgnoreCase("pending") ) {
-                        item.setColour(pendingColour);
-                    }
-                    
-                    if(hdata.containsKey("integratorDemographicId"))
-                    {
-                    	item.setBgColour("#FFCCCC");
-                    	
-                    }
-                }
-                else {
-                    item.setDate(null);
-                }
+        		url += "; return false;";
+        		ArrayList<NavBarDisplayDAO.Item> warnings = new ArrayList<NavBarDisplayDAO.Item>();
+        		ArrayList<NavBarDisplayDAO.Item> items = new ArrayList<NavBarDisplayDAO.Item>();
+        		String result;
+        		Date demographicDateOfBirth=PreventionData.getDemographicDateOfBirth(loggedInInfo, Integer.valueOf(bean.demographicNo));
 
-                String title = StringUtils.maxLenString(h.get("name"),  MAX_LEN_TITLE, CROP_LEN_TITLE, ELLIPSES);
-                item.setTitle(title);
-                item.setLinkTitle(h.get("desc"));
-                item.setURL(url);
+        		// fetch and cache any remote integrated preventions.
+        		List<CachedDemographicPrevention> integratedPreventions = null;
+        		List<CachedDemographicPrevention> remotePreventions = PreventionData.getRemotePreventions(loggedInInfo, demographicNumber);
 
-                //if there's a warning associated with this prevention set item apart
-                if( warningTable.containsKey(prevName) ){
-                    item.setColour(highliteColour);
-                    warnings.add(item);
-                }
-                else {
-                    items.add(item);
-                }
-            }
-        }
+			if(remotePreventions.size() > 0)
+        		{
+                		integratedPreventions = new ArrayList<CachedDemographicPrevention>();
+                		integratedPreventions.addAll(remotePreventions);
+        		}
 
-        //sort items without warnings chronologically
-        Dao.sortItems(items, NavBarDisplayDAO.DATESORT_ASC);
+        		for (int i = 0 ; i < prevList.size(); i++){
+            			boolean flagPrevention = false; 
+				NavBarDisplayDAO.Item item = NavBarDisplayDAO.Item();
+            			HashMap<String,String> h = prevList.get(i);
+            			String prevName = h.get("name");
+            			String displayName = h.get("displayName") != null ? h.get("displayName") : prevName;
+            			ArrayList<Map<String,Object>> alist = PreventionData.getPreventionData(loggedInInfo, prevName, Integer.valueOf(bean.demographicNo));
+            			PreventionData.addRemotePreventions(loggedInInfo, alist, Integer.valueOf(bean.demographicNo),prevName,demographicDateOfBirth);
+            			boolean show = pdc.display(loggedInInfo, h, bean.demographicNo,alist.size());
+            			winName = "addPreventionData";
+            			url = "popupPage(700,960,'" + winName + "', '" + request.getContextPath() + "" + "/oscarPrevention/AddPreventionData.jsp?prevention="+ prevName +"&displayName="+ displayName +"&demographic_no=" + bean.demographicNo + "'); return false;";
+            			if( show ) {
+                			if( alist.size() > 0 ) {
+                    				Map<String,Object> hdata = alist.get(alist.size()-1);
+                    				Map<String,String> hExt = PreventionData.getPreventionKeyValues((String)hdata.get("id"));
+                    				result = hExt.get("result");
 
-        //add warnings to Dao array first so they will be at top of list
-        for(int idx = 0; idx < warnings.size(); ++idx )
-        {
-            Dao.addItem(warnings.get(idx));
-        }
+                    				Object dateObj = hdata.get("prevention_date_asDate");
+                    				if (dateObj instanceof Date){
+                        				date = (Date) dateObj;
+                    				} else if(dateObj instanceof java.util.GregorianCalendar){
+                        				Calendar cal = (Calendar) dateObj;
+                        				date = cal.getTime();
+                    				}
 
-        //now copy remaining sorted items
-        for(int idx = 0; idx < items.size(); ++idx)
-        {
-            Dao.addItem(items.get(idx));
-        }
+                    				item.setDate(date);
 
-        return true;
-    }
-   }
+                    				if( hdata.get("refused") != null && hdata.get("refused").equals("2") ) {
+                        				item.setColour(inelligibleColour);
+							flagPrevention=true;
+                    				}
+                    				else if( result != null && result.equalsIgnoreCase("pending") && !hdata.get("refused").equals("1")) {
+                        				item.setColour(pendingColour);
+							flagPrevention=true;
+                    				}
+                    				else if (hdata.get("refused") != null && hdata.get("refused").equals("1")){
+                        				item.setColour(refusedColour);
+							flagPrevention=true;
+                    				}
+                    				else if (result != null && result.equalsIgnoreCase("abnormal") && !hdata.get("refused").equals("1"))
+                    				{
+                        				item.setColour(abnormalColour);
+							flagPrevention=true;
+                    				}
 
-    public String getCmd() {
-      return cmd;
-    }
+                    				if ( hdata.get("id") != null ) {
+                        				url = "popupPage(700,960,'" + winName + "', '" + request.getContextPath() + "" + "/oscarPrevention/AddPreventionData.jsp?prevention="+ prevName +"&displayName="+ displayName +"&demographic_no=" + bean.demographicNo + "&id=" + hdata.get("id") + "'); return false;";
+                    				}	
+                			}			
+                			else {
+                    				item.setDate(null);
+                			}
+
+                			String title = StringUtils.maxLenString(displayName,  MAX_LEN_TITLE, CROP_LEN_TITLE, ELLIPSES);
+                			item.setTitle(title);
+                			item.setLinkTitle(h.get("desc"));
+                			item.setURL(url);
+
+                			//if there's a warning associated with this prevention set item apart
+                			if( warningTable.containsKey(prevName) && !exclusions.contains(prevName)){
+                    				item.setColour(highliteColour);
+						warnings.add(item);
+					}
+                			else {
+                    				items.add(item);
+                			}
+            			}
+        		}
+
+        		//sort items without warnings chronologically
+        		Dao.sortItems(items, NavBarDisplayDAO.DATESORT_ASC);
+
+                        //add warnings to Dao array first so they will be at top of list
+                        for(int idx = 0; idx < warnings.size(); ++idx )
+                              Dao.addItem(warnings.get(idx));
+   			//now copy remaining sorted items
+                        for(int idx = 0; idx < items.size(); ++idx)
+                               Dao.addItem(items.get(idx));
+
+        		return true;
+    		}
+	}
+
+    	public String getCmd() {
+      		return cmd;
+    	}
 
 }
+
