bd81d6462a49880a229b7c91ede35bfbf0dcfd4c
Peter H-C <phc@srpc.ca>
2022-10-20 13:07:12 +0000

    CaseManagementEntryAction.java

--- src/main/java/org/oscarehr/casemgmt/web/CaseManagementEntryAction.java
+++ src/main/java/org/oscarehr/casemgmt/web/CaseManagementEntryAction.java
@@ -2903,8 +2903,8 @@ public class CaseManagementEntryAction extends BaseCaseManagementEntryAction {
 		if (StringUtils.isNotEmpty(demographic.getSex())) {
 			patientName.append(" ").append(demographic.getSex()).append(" ");
 		}
-		patientName.append(demographic.getYearOfBirth()).append("-").append(demographic.getMonthOfBirth()).append("-").append(demographic.getDateOfBirth);
-		String sPatient = org.owasp.encoder.Encode.forHtml((String) patientName.toString());		
+		patientName.append(demographic.getYearOfBirth()).append("-").append(demographic.getMonthOfBirth()).append("-").append(demographic.getDateOfBirth());
+		String sPatient = Encode.forHtml(patientName.toString());		
 		out.println("<!DOCTYPE html><html><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>" + sStyle + "<title>"+sPatient+"</title></head><body>");
 
 		for (int idx = 0; idx < noteIds.length; ++idx) {
@@ -2925,7 +2925,7 @@ public class CaseManagementEntryAction extends BaseCaseManagementEntryAction {
 			} else {
 				textStr = textStr.replaceAll("\n", "<br>");
 			}
-			out.println(org.owasp.encoder.Encode.forHtml((String) textStr));
+			out.println(Encode.forHtml(textStr));
 			out.println("<br><hr style='border:0; height: 1px;background-image: linear-gradient(to right,rgba(0, 0, 0), rgba(0, 0, 0, 0.6), rgba(0, 0, 0,0));'>");
 			out.println("<br>");
 		}
