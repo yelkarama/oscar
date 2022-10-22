eb41ee73010ae1e608fe152da49cce9736d858b2
Peter H-C <phc@srpc.ca>
2022-10-22 14:19:55 +0000

    CaseManagementEntryAction.java removed unused import java.util.Locale;

--- src/main/java/org/oscarehr/casemgmt/web/CaseManagementEntryAction.java
+++ src/main/java/org/oscarehr/casemgmt/web/CaseManagementEntryAction.java
@@ -38,7 +38,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Locale;
+
 import java.util.Map;
 import java.util.Properties;
 import java.util.ResourceBundle;
