2c11e905d4f2baa00be77e1e9bbfd742d7efefc5
Peter H-C <phc@srpc.ca>
2022-12-06 20:29:05 +0000

    oscarinit_bc.sql BCAR 2020 tables added

--- database/mysql/oscarinit_bc.sql
+++ database/mysql/oscarinit_bc.sql
@@ -2507,6 +2507,39 @@ CREATE TABLE formBCAR2007(
 );
 
 --
+-- Table structure for table 'formBCAR2020'
+--
+CREATE TABLE IF NOT EXISTS `formBCAR2020` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `demographic_no` int(10) NOT NULL,
+  `provider_no` varchar(6) NOT NULL,
+  `formCreated` date DEFAULT NULL,
+  `formEdited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `id` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `formBCAR2020Data` (
+  `form_id` int(10) NOT NULL,
+   `provider_no` varchar(6) NOT NULL,
+  `page_no` int(1) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `val` varchar(255) NOT NULL DEFAULT '',
+  `field_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `form_data` (`form_id`,`page_no`,`field`(20))
+);
+
+CREATE TABLE IF NOT EXISTS `formBCAR2020Text` (
+  `form_id` int(10) NOT NULL,
+  `provider_no` varchar(6) NOT NULL,
+  `page_no` int(1) NOT NULL,
+  `field` varchar(255) NOT NULL,
+  `val` text NOT NULL,
+  `field_edited` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `form_data` (`form_id`,`page_no`,`field`(20))
+);
+
+
+--
 -- Table structure for table 'formBCBirthSumMo'
 --
 
