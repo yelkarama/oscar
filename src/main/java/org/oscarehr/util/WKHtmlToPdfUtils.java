feb2c44c0195570c1070a41d84483e29378cc655
Peter H-C <phc@srpc.ca>
2022-12-23 21:42:29 +0000

    WKHtmlToPdfUtils.java unused import commented out

--- src/main/java/org/oscarehr/util/WKHtmlToPdfUtils.java
+++ src/main/java/org/oscarehr/util/WKHtmlToPdfUtils.java
@@ -26,7 +26,7 @@ package org.oscarehr.util;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+//import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
