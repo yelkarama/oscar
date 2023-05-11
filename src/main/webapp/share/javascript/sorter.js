6010d0db2cdc28b6987ba42865f2fcfe5371ffd8
Peter H-C <phc@srpc.ca>
2023-05-11 17:31:47 +0000

    sorter.js to match openoscar

--- src/main/webapp/share/javascript/sorter.js
+++ src/main/webapp/share/javascript/sorter.js
@@ -24,7 +24,7 @@ function resizeUl() {
 	var sum = 0;
 	//$("#picker li").each(function() { sum += $(this).width(); } );
 	//$("#picker").width(sum+3);
-	
+
 	sum = 0;
 	$("#builder li").each(function() { sum += $(this).width(); } );
 	if (sum == 0) $("#builder").width('99%');
@@ -53,7 +53,7 @@ function _rotater(e) {
 
 	var r = parseInt($(e).attr("rotate"));
 	r = (r != "NaN" ? r : 0);
-	
+
 	if (90 + r >= 360) r = -90;
 	$(e).attr("rotate", 90 + r);
 	$(e).parent().addClass("rotated");
@@ -69,28 +69,28 @@ function _resizeui() {
 function _zoom(d) {
 	var img = $(d).find('img').attr('src');
 	var modal = $('img[src$="' + img + '"]').clone();
-	
+
 	var t = new Image();
 	t.src = img;
-	
+
 	var height = t.height;
 	var width = t.width;
-	
+
 	modal.dialog({
 		height: $(window).height() - 40,
 		modal: true,
 		draggable: false,
 		resizable: false,
-		width: width 
+		width: width
 	});
 }
 
 function _remove(r) {
-	
+
 	if ($(r).parent().parent().parent().attr('id') == "builder") {
 		var rem = $(r).parent();
 		$("#picker").append($(rem).parent());
-		
+
 		clearPreviousSelection();
 		_resetsortable();
 	}
@@ -99,7 +99,7 @@ function _remove(r) {
 function _resetsortable() {
 	/*
 	$("#picker li").sortable("destroy");
-	
+
 	$("#picker li").sortable({
 		helper: 'clone',
 		connectWith: '#builder',
@@ -128,35 +128,35 @@ $(document).ready(function() {
 	//}).mouseout(function() {
 	//	this.style.height = "";
 	//});
-	
+
 	$("#picker div").append(function(index, html) {
 		return "<span class='num'>" + ++index + "</span>" +
-				"<span class='jog-control'><img style='height: 16px; -box-shadow: none; " +
+				"<span class='jog-control'><img style='-box-shadow: none; " +
 				"-webkit-box-shadow: none; -moz-box-shadow: none' src='../images/icons/132.png' />" +
-				"<img style='height: 16px; -box-shadow: none; -webkit-box-shadow: none; " +
+				"<img style=' -box-shadow: none; -webkit-box-shadow: none; " +
 				"-moz-box-shadow: none' src='../images/icons/131.png' />" +
-				"<img style='height: 16px; -box-shadow: none; -webkit-box-shadow: none; " +
+				"<img style=' -box-shadow: none; -webkit-box-shadow: none; " +
 				"-moz-box-shadow: none' src='../images/icons/114.png' /></span>";
 	});
-	
+
 	//$("#picker div span.rotate_r").click(function() { _rotater(this) });
-	
+
 	$(document).bind("contextmenu", function(e) { return false; } );
-	
+
 	$(window).resize(function() {
 		_resizeui();
 	});
-	
+
 	_resizeui();
 	//_resetsortable();
-	
+
 	$("#picker div").click(function(e) {
 		$(this).addClass("selected");
 		if ($(this).parent().attr('id') == "builder")
 			clearPreviousSelection();
 		else if ($(this).parent().attr('id') == "picker")
 			clearPreviousTopSelection();
-			
+
 		if (e.shiftKey) {
 			selected.push(this);
 		} else {
@@ -164,14 +164,14 @@ $(document).ready(function() {
 			selected = [this];
 		}
 	}).hover(function(e) {
-		
+
 	});
-	
+
 	$("#picker div").dblclick(function(e) {
 		_zoom(this);
 	});
-	
-	
+
+
 	/*$("#builder").sortable({
 		helper: 'clone',
 		revert: 'invalid',
@@ -196,21 +196,21 @@ $(document).ready(function() {
 	}).droppable({
 		activeClass: 'accept-drop'
 	});*/
-	
+
 	$("#tool_add").click(function(e) {
 		if ($(selected).length > 0) {
 			var selectedp = $(selected).parent();
 			for (var s = 0; s < $(selectedp).length; s++)
 				$("#builder").append(selectedp[s]);
-			
+
 			$("#picker").remove($(selected).parent());
 			selected = [];
 			$(".selected").removeClass("selected");
-			
+
 			resizeUl();
 		}
 	});
-	
+
 	$("#tool_remove").click(function(e) {
 		var selectedr = $(selected).find("img, canvas");
 		if ($(selectedr).length > 0) {
@@ -218,53 +218,50 @@ $(document).ready(function() {
 				_remove(selectedr[s]);
 		}
 	});
-	
+
 	$("#tool_rotate").click(function(e) {
 		if ($(selected).length > 0) {
 			for (var s = 0; s < $(selected).length; s++)
 				_rotater(selected[s]);
 		}
 	});
-	
+
 	$("#tool_savecontinue").click(function(e) {
 		$("#builder").children().each(function() {
 			var num = $(this).find("span").html();
 			var rotate = $(this).find("div").attr("rotate");
 			$(this).attr("id", "page_" + num + "," + rotate);
 		});
-		
+
 		$("#builder").sortable();
-		var serialized = $("#builder").sortable('serialize');
+		var serialized = $("#builder").sortable('serialize',{key : 'page'});
+		serialized = encodeURI(serialized);
 		$("#builder").sortable("destroy");
 		$("#builder").empty();
-		
+
 		var docnum = $("#document_no").attr('value');
 		var queueId = $("#queueID").attr('value');
 		var demoName = $("#demoName").attr('value');
-		
+
 		$("#tool_savecontinue span").html("Wait...");
-		
-		$.getJSON('../dms/SplitDocument.do?method=split&document=' + docnum + '&' + serialized + '&queueID=' + queueId,			
+
+		$.getJSON(ctx + '/dms/SplitDocument.do?method=split&document=' + docnum + '&' + serialized + '&queueID=' + queueId,
 				function(data) {
 					$("#tool_savecontinue span").html("Save &amp; Continue");
-					popup(screen.height,screen.width,"../dms/showDocument.jsp?segmentID="+data["newDocNum"] + '&demoName=' + encodeURIComponent(demoName) + "&inWindow=true", "assignDoc");			
+					popup(screen.height,screen.width, ctx + "/dms/showDocument.jsp?segmentID="+data["newDocNum"] + '&demoName=' + encodeURIComponent(demoName) + "&inWindow=true", "assignDoc");
 				return false;
 			}
 		);
 	});
-	
+
 	$("#tool_done").click(function(e) {
-		
+
 		if( confirm("Are you sure want to exit?")) {
-		
-			var docnum = $("#document_no").attr('value');
-			
-			opener.refreshAndFile(docnum);
 			window.close();
 		}
-		
+
 	});
-	
+
 	$(".jog-control").find(">:first-child").click(function(e) {
 		// "prev" button
 		var li = $(this).parent().parent().parent();
@@ -273,20 +270,20 @@ $(document).ready(function() {
 			$("#builder li:eq(" + (index - 1) + ")").before(li);
 		}
 	});
-	
+
 	$(".jog-control").find(">:first-child").next().click(function(e) {
 		// "next" button
 		var li = $(this).parent().parent().parent();
 		var index = $(li).index();
 		$("#builder li:eq(" + (index + 1) + ")").after(li);
 	});
-	
+
 	$(".jog-control").find(">:first-child").next().next().click(function(e) {
 		// "rotate" button
 		var div = $(this).parent().parent();
 		_rotater(div);
 	});
-	
+
 	$(".jog-control").find("img").hover(function(e) {
 		$(this).css("opacity", "1.0");
 	}, function(e) {
